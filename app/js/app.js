67289ff01fb03e472837c682191d66af3cac6a6a
andrewcodes404@gmail.com <andrewcodes404@gmail.com>
2018-06-21 14:29:09 +0100

    added fullpage.js

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,5 @@
-const whatsupdoc = getElementsByClassName('.any-div-test')
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6'],
+  });
+});
